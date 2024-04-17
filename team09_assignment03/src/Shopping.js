810cad1b3a26384659aaca3fb1dd0cd1358fb283
Mattie <mrm4@iastate.edu>
2024-04-17 12:36:51 -0500

    Created a blank app with everything installed for assignment 3

---
+++ team09_assignment03/src/Shopping.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.css";
+
+
+const Shop = () => {
+    return (
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="error-container">
+                            <h1 class="display-4">404</h1>
+                            <p class="lead">Page Not Found</p>
+                            <p>Sorry, the page you are looking for does not exist.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Shop;

464b8419ebef266f630e4e177792b258cd4dd6a1
jthuaa <157547300+jthuaa@users.noreply.github.com>
2024-04-22 11:00:10 -0500

    Update app_backend.js

--- team09_assignment03/src/app_backend.js
+++ team09_assignment03/src/app_backend.js
@@ -10,6 +10,38 @@ app.use(bodyParser.json());
 const port = "8081";
 const host = "localhost";
 
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "reactdata";
+const client = new MongoClient(url);
+const db = client.db(dbName);
+
 app.listen(port, () => {
     console.log("App listening at http://%s:%s", host, port);
 });
+
+app.get("/listProducts", async (req, res) => {
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
+    const query = {};
+    const results = await db
+    .collection("fakestore_catalog")
+    .find(query)
+    .limit(100)
+    .toArray();
+    console.log(results);
+    res.status(200);
+    res.send(results);
+    });
+
+app.get("/:id", async (req, res) => {
+    const productid = Number(req.params.id);
+    await client.connect();
+    console.log("Node connected successfully to GET-id MongoDB");
+    const query = {"id": productid };
+    const results = await db.collection("fakestore_catalog")
+    .findOne(query);
+    console.log("Results :", results);
+    if (!results) res.send("Not Found").status(404);
+    else res.send(results).status(200);
+    
+    });
